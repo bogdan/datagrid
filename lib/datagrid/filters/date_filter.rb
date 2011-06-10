f4107500c8b5448c284e1a5815cf7c40dd185145
Bogdan Gusiev <agresso@gmail.com>
2011-06-10 17:33:29 +0300

    DateFilter#format improved

--- lib/datagrid/filters/date_filter.rb
+++ lib/datagrid/filters/date_filter.rb
@@ -1,8 +1,10 @@
 class Datagrid::Filters::DateFilter < Datagrid::Filters::BaseFilter
   #TODO: more smart date normalizer
   def format(value)
+    return nil if value.blank?
+    return value.to_date if value.respond_to?(:to_date)
     return value unless value.is_a?(String)
-    value.blank? ? nil : Date.parse(value)
+    Date.parse(value)
   rescue ArgumentError
     nil
   end
