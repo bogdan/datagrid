53816e6a089615476bca632c7f10dd0221889f4e
Jean-Francois Turcot <jfturcot@calypsoconcept.com>
2011-08-20 02:28:27 -0400

    Added a prompt option to enum filter

--- lib/datagrid/filters/enum_filter.rb
+++ lib/datagrid/filters/enum_filter.rb
@@ -15,9 +15,12 @@ class Datagrid::Filters::EnumFilter < Datagrid::Filters::BaseFilter
     option.respond_to?(:call) ? option.call : option
   end
 
-
   def include_blank
-    self.options.has_key?(:include_blank) ? options[:include_blank] : true
+    self.options.has_key?(:include_blank) ? options[:include_blank] : true unless self.prompt
+  end
+  
+  def prompt
+    self.options.has_key?(:prompt) ? options[:prompt] : false
   end
 
   def strict
