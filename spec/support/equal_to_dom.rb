b3715008feeda561885ac0426e13319dfa546ef7
Jakob Holderbaum <jh@neopoly.de>
2011-11-26 13:59:24 +0100

    Add CssPattern rspec-matcher.
    This matcher is required to test datagrid_table in the helper test.
    The extractet renderer uses its own methods, so the stubbed order_for
    helper is not called at all.
    
    The matches against css-pathes gives more flexibility in partial testing
    of the generated output.

--- spec/support/equal_to_dom.rb
+++ spec/support/equal_to_dom.rb
@@ -9,6 +9,11 @@ def include_dom(text)
   IncludeDom.new(text)
 end
 
+def match_css_pattern(pattern)
+  CssPattern.new(pattern)
+end
+
+
 class IncludeDom
   def initialize(expectation)
     @expectation = Nokogiri::HTML::DocumentFragment.parse(expectation).to_s
@@ -39,4 +44,48 @@ class EqualToDom
   def failure_message
     "Expected dom #{@matcher} to match #{@expectation}, but it wasn't"
   end
+end
+
+
+class CssPattern
+  def initialize(pattern)
+    @css_pattern = pattern
+  end
+
+  def error!(message)
+    @error_message = message
+    false
+  end
+
+  def failure_message
+    @error_message || ""
+  end
+
+  def matches?(text)
+    @matcher = Nokogiri::HTML::DocumentFragment.parse(text)
+    @css_pattern.each do |css, amount_or_pattern_or_string_or_proc|
+      path = @matcher.css(css)
+      if amount_or_pattern_or_string_or_proc.is_a?(String) or amount_or_pattern_or_string_or_proc.is_a?(Regexp)
+        pattern_or_string = amount_or_pattern_or_string_or_proc 
+        html = path.inner_html
+        if !html.match(pattern_or_string)
+          return error!("#{css.inspect} did not match #{pattern_or_string.inspect}. It was \n:#{html.inspect}")
+        end
+      elsif amount_or_pattern_or_string_or_proc.is_a? Fixnum
+        expected_amount = amount_or_pattern_or_string_or_proc
+        amount = path.size
+        if amount != expected_amount
+          return error!("did not find #{css.inspect} #{expected_amount.inspect} times. It was #{amount.inspect}")
+        end
+      elsif amount_or_pattern_or_string_or_proc.is_a? Proc
+        if !amount_or_pattern_or_string_or_proc.call(path)
+          return error!("#{css.inspect} did not validate (proc must not return a falsy value)")
+        end
+      else
+        raise "Instance of String, Rexexp, Proc or Fixnum required"
+      end
+      true
+    end 
+  end
+
 end
