9d647ea598991f970c7aa6954168dc213ef6d728
Bogdan Gusiev <agresso@gmail.com>
2011-12-15 10:53:10 +0200

    Remove code dups

--- lib/datagrid/helper.rb
+++ lib/datagrid/helper.rb
@@ -5,27 +5,30 @@ module Datagrid
   module Helper
 
     def datagrid_format_value(report, column, asset)
-      Renderer.for(self).format_value(report, column, asset)
+      datagrid_renderer.format_value(report, column, asset)
     end
 
     def datagrid_table(report, *args)
-      Renderer.for(self).table(report, *args)
+      datagrid_renderer.table(report, *args)
     end
 
     def datagrid_header(grid, options = {})
-      Renderer.for(self).header(grid, options)
+      datagrid_renderer.header(grid, options)
     end
 
     def datagrid_rows(report, assets, options = {})
-      Renderer.for(self).rows(report, assets, options)
+      datagrid_renderer.rows(report, assets, options)
     end
 
     def datagrid_order_for(grid, column)
-      Renderer.for(self).order_for(grid, column)
+      datagrid_renderer.order_for(grid, column)
     end
 
     protected
 
+    def datagrid_renderer
+      Renderer.for(self)
+    end
 
     def datagrid_render_column(column, asset)
       instance_exec(asset, &column.block)
