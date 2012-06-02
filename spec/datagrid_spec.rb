a8d3ca543aadb6ba86be1dbc632d5fe6922721be
Bogdan Gusiev <agresso@gmail.com>
2012-06-02 16:57:50 +0300

    Revert "Adding 'dynamic parameters for the scope value' - Issue #28Adding 'dynamic parameters for the scope value' - Issue #28"
    This reverts commit 389fe70cab8df732eb3105b2502e290ff559ea0e.
    
    Conflicts:
    
    	spec/datagrid_spec.rb

--- spec/datagrid_spec.rb
+++ spec/datagrid_spec.rb
@@ -3,11 +3,11 @@ require "datagrid/rspec"
 
 
 describe SimpleReport do
-
+  
   it_should_behave_like 'Datagrid'
-
+  
   let(:group) { Group.create!(:name => "Pop") }
-
+  
   subject do
     SimpleReport.new(
       :group_id => group.id,
@@ -37,6 +37,7 @@ describe SimpleReport do
     it "should return defined scope of objects" do
       subject.scope.should respond_to(:each)
     end
+      
 
     context "when not defined on class level" do
       subject do
@@ -52,32 +53,6 @@ describe SimpleReport do
     end
   end
 
-end
 
-class SimpleReportWithContext < SimpleReport
-  attr_accessor :current_user
-  scope do
-    current_user.things
-  end
-end
-
-describe SimpleReportWithContext, "with context" do
-
-  subject do
-
-    SimpleReportWithContext.new(
-      :name => "Star",
-      :category => "first",
-      :disabled => false,
-      :confirmed => false
-    )
-  end
-
-  it "should call " do
-    current_user = Object.new
-    current_user.should_receive(:things).and_return([])
-    SimpleReportWithContext.new(:current_user => current_user).scope
-  end
 
 end
-
