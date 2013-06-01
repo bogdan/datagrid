0bf60f703e7a6808dda8d10e0e4b082b64aed68b
Bogdan Gusiev <agresso@gmail.com>
2013-06-01 18:28:43 +0300

    Docs

--- lib/datagrid/helper.rb
+++ lib/datagrid/helper.rb
@@ -4,32 +4,71 @@ require "action_view"
 module Datagrid
   module Helper
 
-    def datagrid_format_value(report, column, asset)
-      datagrid_renderer.format_value(report, column, asset)
+    # Format an value from datagrid column with given name and for given model
+    def datagrid_format_value(report, column, model)
+      datagrid_renderer.format_value(report, column, model)
     end
 
+    # Renders html table with columns defined in grid class.
+    # In the most common used you need to pass paginated collection 
+    # to datagrid table because datagrid do not have pagination compatibilities:
+    #     
+    #   assets = grid.assets.page(params[:page])
+    #   datagrid_table(grid, assets, options)
+    #
+    # Supported options:
+    #
+    # * <tt>:html</tt> - hash of attributes for <table> tag
+    # * <tt>:order</tt> - If false do not generate ordering controlls. 
+    #   Default: true.
+    # * <tt>:cycle</tt> - Used as arguments for cycle for each row. 
+    #   Default: false. Example: <tt>["odd", "even"]</tt>.
+    # * <tt>:columns</tt> - Array of column names to display. 
+    #   Used in case when same grid class is used in different places 
+    #   and needs different columns. Default: all defined columns.
     def datagrid_table(report, *args)
       datagrid_renderer.table(report, *args)
     end
 
+    # Renders HTML table header for given grid instance using columns defined in it
+    #
+    # Supported options:
+    #
+    # * <tt>:order</tt> - display ordering controls built-in into header
+    #   Default: true
+    #     
     def datagrid_header(grid, options = {})
       datagrid_renderer.header(grid, options)
     end
 
+
+    # Renders HTML table rows using given grid definition using columns defined in it
     def datagrid_rows(report, assets, options = {})
       datagrid_renderer.rows(report, assets, options)
     end
 
+    # Renders ordering controls for the given column name
     def datagrid_order_for(grid, column)
       datagrid_renderer.order_for(grid, column)
     end
 
+    # Renders HTML for for grid with all filters inputs and lables defined in it
     def datagrid_form_for(grid, options = {})
       datagrid_renderer.form_for(grid, options)
     end
 
-    def datagrid_row(report, asset, &block)
-      HtmlRow.new(self, report, asset).tap do |row|
+    # Provides access to datagrid column data.
+    #
+    #   <%= datagrid_row(grid, user) do |row| %>
+    #     <tr>
+    #       <td><%= row.first_name %></td>
+    #       <td><%= row.last_name %></td>
+    #     </tr>
+    #   <% end %>
+    #
+    # Used in case you want to build datagrid table completelly manually
+    def datagrid_row(grid, asset, &block)
+      HtmlRow.new(self, grid, asset).tap do |row|
         if block_given?
           return capture(row, &block)
         end
