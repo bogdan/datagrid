1202a25dd76634c6ddd91a5547651bfaba30944c
Bogdan Gusiev <agresso@gmail.com>
2013-11-01 13:22:46 +0200

    Bugfix select filter with checkboxes

--- lib/datagrid/form_builder.rb
+++ lib/datagrid/form_builder.rb
@@ -40,7 +40,7 @@ module Datagrid
         options[:multiple] = true
       end
       if filter.checkboxes?
-        filter.select(options).map do |element|
+        filter.select(object).map do |element|
           text, value = @template.send(:option_text_and_value, element)
           id = [object_name, filter.name, value.underscore].join('_')
           input = check_box(filter.name, options.merge(datagrid_extra_checkbox_options).reverse_merge(:id => id, :multiple => true), value, nil)
