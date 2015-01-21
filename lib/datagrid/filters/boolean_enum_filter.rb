f1cf2140e3a52818e19390e190781f0baa727103
Bogdan Gusiev <agresso@gmail.com>
2015-01-21 17:12:23 +0200

    Remove deprecated eboolean localizations

--- lib/datagrid/filters/boolean_enum_filter.rb
+++ lib/datagrid/filters/boolean_enum_filter.rb
@@ -5,7 +5,7 @@ class Datagrid::Filters::BooleanEnumFilter < Datagrid::Filters::EnumFilter #:nod
 
   def initialize(report, attribute, options = {}, &block)
     options[:select] = [YES, NO].map do |key, value|
-      [I18n.t("datagrid.filters.eboolean.#{key.downcase}"), key]
+      [I18n.t("datagrid.filters.eboolean.#{key.downcase}", :default => key.downcase.capitalize), key]
     end
     super(report, attribute, options, &block)
   end
