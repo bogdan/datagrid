132b93c7206bb6dd86e51c834acf4d66ddc51082
Bogdan Gusiev <agresso@gmail.com>
2015-03-26 18:29:10 +0200

    Refactor translation module

--- lib/datagrid/translation.rb
+++ lib/datagrid/translation.rb
@@ -2,23 +2,19 @@ require "action_view/helpers"
 
 module Datagrid
   module Translation
-    include ActionView::Helpers::TranslationHelper
 
     def translate_from_namespace(namespace, grid_class, key)
-      deprecated_translation_key = :"#{grid_class.param_name}.#{namespace}.#{key}"
+      deprecated_key = :"datagrid.#{grid_class.param_name}.#{namespace}.#{key}"
+      live_key = :"datagrid.#{grid_class.model_name.i18n_key}.#{namespace}.#{key}"
+      i18n_key = grid_class.model_name.i18n_key.to_s
 
-      if grid_class.name.include?("::") && I18n.exists?(deprecated_translation_key)
+      if grid_class.param_name != i18n_key && I18n.exists?(deprecated_key)
         Datagrid::Utils.warn_once(
-          "Deprecated translation namespace. Use 'datagrid.#{grid_class.model_name.i18n_key}' instead"
+          "Deprecated translation namespace. Use 'datagrid.#{i18n_key}' instead"
         )
+        return I18n.t(deprecated_key)
       end
-
-      lookups = [
-        :"#{grid_class.model_name.i18n_key}.#{namespace}.#{key}",
-        deprecated_translation_key,
-        key.to_s.humanize
-      ]
-      t(lookups.shift, scope: "datagrid", default: lookups).presence
+      I18n.t(live_key, default: key.to_s.humanize).presence
     end
 
   end
