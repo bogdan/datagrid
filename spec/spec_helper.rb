34cb6bd6d9049d89fafe27833c64ab0c78d0fb86
Bogdan Gusiev <agresso@gmail.com>
2015-09-21 17:24:35 +0300

    Improve mongo drivers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,6 +99,4 @@ end
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/schema.rb"].each {|f| require f}
-Dir["#{File.dirname(__FILE__)}/support/mongoid.rb"].each {|f| require f}
-Dir["#{File.dirname(__FILE__)}/support/mongo_mapper.rb"].each {|f| require f}
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
