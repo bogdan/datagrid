42241dc8e49963a2ede65ee83e95a6b8e5fdb1e9
Bogdan Gusiev <agresso@gmail.com>
2016-03-30 18:34:55 +0300

    Update scaffold to support existing controller

--- lib/datagrid/scaffold.rb
+++ lib/datagrid/scaffold.rb
@@ -1,11 +1,19 @@
 require "rails/generators"
 
 class Datagrid::Scaffold < Rails::Generators::NamedBase
+
+  include Rails::Generators::ResourceHelpers
+
+  check_class_collision :suffix => "Grid"
   source_root File.expand_path(__FILE__ + "/../../../templates")
 
   def create_scaffold
     template "grid.rb.erb", "app/grids/#{grid_class_name.underscore}.rb"
-    template "controller.rb.erb", "app/controllers/#{grid_controller_name.underscore}.rb"
+    if File.exists?(grid_controller_file)
+      inject_into_file grid_controller_file, index_action, :after => %r{class .*#{grid_controller_name}.*\n}
+    else
+      template "controller.rb.erb", grid_controller_file
+    end
     template "index.html.erb", "app/views/#{grid_controller_short_name}/index.html.erb"
     route("resources :#{grid_controller_short_name}")
     unless defined?(::Kaminari) || defined?(::WillPaginate)
@@ -28,9 +36,13 @@ class Datagrid::Scaffold < Rails::Generators::NamedBase
   def grid_class_name
     file_name.camelize.pluralize + "Grid"
   end
-  
+
   def grid_controller_name
     grid_controller_short_name.camelize + "Controller"
+  end
+
+  def grid_controller_file
+    Rails.root.join("app/controllers/#{grid_controller_name.underscore}.rb")
   end
 
   def grid_controller_short_name
@@ -41,12 +53,8 @@ class Datagrid::Scaffold < Rails::Generators::NamedBase
     file_name.camelize.singularize
   end
 
-  def grid_ivar_name
+  def grid_param_name
     grid_class_name.underscore
-  end
-
-  def paginate_code
-    "page(params[:page])"
   end
 
   def pagination_helper_code
@@ -61,6 +69,16 @@ class Datagrid::Scaffold < Rails::Generators::NamedBase
 
   def grid_route_name
     grid_controller_short_name + "_path"
+  end
+
+  def index_action
+    indent(<<-RUBY)
+def index
+  @grid = #{grid_class_name}.new(params[:#{grid_param_name}]) do |scope|
+    scope.page(params[:page])
+  end
+end
+RUBY
   end
 
 end
--- spec/datagrid/scaffold_spec.rb
+++ spec/datagrid/scaffold_spec.rb
@@ -1,30 +1,39 @@
 require 'spec_helper'
 
 describe Datagrid::Scaffold do
-  subject { Datagrid::Scaffold.new([""]) }
+  subject { Datagrid::Scaffold.new(["user"]) }
 
-  describe '.paginate_code' do
-    it 'should fall through options successfully' do
-      expect(subject.paginate_code).to eql('page(params[:page])')
-    end
-  end
 
   describe '.pagination_helper_code' do
-    it 'should fall through options successfully' do
+    it 'uses kaminari by default' do
       expect(subject.pagination_helper_code).to eql('paginate(@grid.assets)')
     end
 
-    context "when Kaminari exists" do
+    context "when WillPaginate exists" do
       before(:each) do
-        Object.const_set("Kaminari", 1)
+        Object.const_set("WillPaginate", 1)
       end
-      it 'should fall through options successfully' do
-        expect(subject.pagination_helper_code).to eql('paginate(@grid.assets)')
+      it 'uses willpaginate' do
+        expect(subject.pagination_helper_code).to eql('will_paginate(@grid.assets)')
       end
 
       after(:each) do
-        Object.send(:remove_const, "Kaminari")
+        Object.send(:remove_const, "WillPaginate")
       end
     end
+  end
+
+  describe ".index_action" do
+
+    it "works" do
+      expect(subject.index_action).to eq(<<-RUBY)
+  def index
+    @grid = UsersGrid.new(params[:users_grid]) do |scope|
+      scope.page(params[:page])
+    end
+  end
+RUBY
+    end
+
   end
 end
