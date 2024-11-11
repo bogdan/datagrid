dbe6600190682518eb506136111b4e86c424cef6
Bogdan Gusiev <agresso@gmail.com>
2024-11-11 10:57:05 +0100

    Fix script bug

--- version-2/find_broken_range_filters.rb
+++ version-2/find_broken_range_filters.rb
@@ -6,7 +6,11 @@ raise "Use version 2" if Datagrid::VERSION < "2.0.0"
 included_classes = ObjectSpace.each_object(Class).select do |klass|
   klass.included_modules.include?(Datagrid)
 end
-classes = [*included_classes, *Datagrid::Base.subclasses].uniq
+
+base_subclasses = ObjectSpace.each_object(Class).select do |klass|
+  klass < Datagrid::Base
+end
+classes = [*included_classes, *base_subclasses].uniq
 
 classes.flat_map(&:filters).select do |f|
   f.respond_to?(:range?) && f.range? && f.block
