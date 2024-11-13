f08ed54336fa231a4c750afb9ebc4347c78a8e03
Bogdan Gusiev <agresso@gmail.com>
2024-11-13 10:45:13 +0100

    Add boolean filter tests

---
+++ spec/datagrid/filters/boolean_filter_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Datagrid::Filters::BooleanFilter do
+  it "applies default filtering" do
+    grid = test_report do
+      scope { Entry }
+      filter(:disabled, :boolean)
+    end
+
+    disabled_entry = Entry.create!(disabled: true)
+    enabled_entry = Entry.create!(disabled: false)
+
+    expect(grid.disabled).to be(false)
+    expect(grid.assets).to include(disabled_entry, enabled_entry)
+    grid.disabled = true
+
+    expect(grid.disabled).to eq(true)
+    expect(grid.assets).to include(disabled_entry)
+    expect(grid.assets).not_to include(enabled_entry)
+    grid.disabled = false
+    expect(grid.disabled).to eq(false)
+    expect(grid.assets).to include(enabled_entry)
+    expect(grid.assets).to include(disabled_entry)
+  end
+
+  it "type casts any value to boolean" do
+    grid = test_report do
+      scope { Entry }
+      filter(:disabled, :boolean)
+    end
+
+    grid.disabled = true
+    expect(grid.disabled).to eq(true)
+
+    grid.disabled = false
+    expect(grid.disabled).to eq(false)
+
+    grid.disabled = 1
+    expect(grid.disabled).to eq(true)
+
+    grid.disabled = 0
+    expect(grid.disabled).to eq(false)
+
+    grid.disabled = "true"
+    expect(grid.disabled).to eq(true)
+
+    grid.disabled = "false"
+    expect(grid.disabled).to eq(false)
+  end
+end
